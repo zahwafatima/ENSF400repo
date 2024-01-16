20932f87b93e7c0e539750466e7ed2ae9480b576
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2024-01-16 11:41:37 -0700

    more lab

--- lab0c.py
+++ lab0c.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
 
+num1 = 5
+num2 = 10
+
+print(num1)
+print(num2)
+
+sum = num1 + num2
+
+print(sum)
+
+print('The sum is: ' + str(sum))
+
+
+name = 'Isaac'
+age = 72
+print(name + ' is ' + str(age) + ' years old!')
+
+print(10 + 5)   # addition
+print(10 - 5)   # subtraction
+print(10 * 5)   # multiplication
+print(10 / 5)   # division
+print(10 ** 5)  # exponents
+
+x = 10
+y = 2
+z = 5
+
+sum = x + y * 5
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(sum))
