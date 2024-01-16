1c89fb3627e777c6d93ae46055a532bc165d847f
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2024-01-16 11:29:55 -0700

    finish this part

--- lab0b.py
+++ lab0b.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+# 2
 name = 'Thomas'
 
+# 3
 print(name)
 
+# 4
 print('name')
 
+# 5
 print('I have a friend named ' + name)
+
+# 6
+name2 = 'Isaac'
+
+print('How old are you ' + name2)
